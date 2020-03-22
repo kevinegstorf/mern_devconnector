0afe6ef28236f59bd914bf37fa184f89364d9617
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2020-03-22 21:33:56 +0100

    added db settings and basic express

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("HELLO HELLO"));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port http://localhost:${PORT}/`);
+});
